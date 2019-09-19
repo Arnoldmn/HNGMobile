47e07af892f4149d3d8b48ae0e87e9c3f9ee9a94
Mumbere Arnold <arnoldnzibuk@gmail.com>
2019-09-19 22:14:05 +0300

    Finishing login logic

--- app/src/main/java/mna/mumbere/hngmobile/LoginActivity.kt
+++ app/src/main/java/mna/mumbere/hngmobile/LoginActivity.kt
@@ -1,12 +1,40 @@
 package mna.mumbere.hngmobile
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val sharedPreferences = getSharedPreferences("SP_INFO", Context.MODE_PRIVATE)
+        val isLoggedIn: Boolean = sharedPreferences.getBoolean("isLoggedIn", false)
+
+        val email = sharedPreferences.getString("Email", "DEFAULT_EMAIL")
+        val password = sharedPreferences.getString("Password", "DEFAULT_PASSWORD")
+
+        if (isLoggedIn){
+            val loginIntent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(loginIntent)
+        }
+
+        btnLogin.setOnClickListener{
+
+
+            val mEmail = txtEmail.text.toString().trim()
+            val mPassword = txtPassword.toString().trim()
+
+            if (email === mEmail && password == mPassword){
+                sharedPreferences.edit().putBoolean("isLoggedIn", false).apply()
+            }else{
+                Toast.makeText(applicationContext,"Email address or password invalid!",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
